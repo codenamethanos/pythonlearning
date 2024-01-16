27bfc8e65b6dbfcc56713cddb14ce76bb26c0834
codenamethanos <harebrasheedadm@gmail.com>
2024-01-16 20:43:24 +0530

    X

---
+++ example4.py
@@ -0,0 +1,19 @@
+class Point:
+    def move(self):
+        print("move")
+    
+    def draw(self):
+        print("draw")
+    
+
+point1 = Point()
+point1.x = 10
+point1.y = 20
+print(point1.x)
+point1.draw()
+
+point2 = Point()
+point2.x = 15
+point2.y = 25
+print(point2.x)
+point2.draw()
