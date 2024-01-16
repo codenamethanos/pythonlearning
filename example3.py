d8461dd42fecbdfdd0eb88d44d094431e3a10ab6
codenamethanos <harebrasheedadm@gmail.com>
2024-01-16 20:34:15 +0530

    X

--- example3.py
+++ example3.py
@@ -18,3 +18,20 @@ customer = {
 
 print(customer["name"]) 
 print(customer.get("name", "Default Value"))
+
+
+try:
+    age = int(input("Age: "))
+    print(age)
+except ValueError:
+    print("Invalid value")
+
+try:
+    age = int(input("Age: "))
+    income = 20000
+    risk = income / age
+    print(age)
+except ZeroDivisionError:
+    print("Age cannot be 0") 
+except ValueError:
+    print("Invalid value")
