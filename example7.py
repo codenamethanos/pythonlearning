908b9005711f8eb7f608bb19d7ff0a781f55bc66
codenamethanos <harebrasheedadm@gmail.com>
2024-01-17 12:47:13 +0530

    X

---
+++ example7.py
@@ -0,0 +1,9 @@
+# import ecommerce.shipping
+# ecommerce.shipping.calc_shipping()
+
+# from ecommerce.shipping import calc_shipping, calc_tax
+# calc_shipping()
+# calc_tax()
+
+from ecommerce import shipping
+shipping.calc_shipping()
