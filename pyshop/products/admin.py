3a0dd6b07e95226918bea40ce7df1425a4c2db62
codenamethanos <harebrasheedadm@gmail.com>
2024-01-21 22:27:55 +0530

    X

--- pyshop/products/admin.py
+++ pyshop/products/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Offer
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'stock')
 
-admin.site.register(Product, ProductAdmin)
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'description', 'discount')
 
-# Register your models here.
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Offer, OfferAdmin)
